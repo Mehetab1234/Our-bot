ab774d21af816324b9f90c8d3e902ae4bebd853d
CRAFTINGCRAZEGAMIMG <mehetabhoque19@gmail.com>
2025-03-16 14:15:53 +0530

    Update keep.alive.py

--- keep.alive.py
+++ keep.alive.py
@@ -1,14 +1,15 @@
+
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Discord Bot is alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
